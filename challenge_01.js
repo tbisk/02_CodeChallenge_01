a31ed82e6f08abd33bb83da33eaabd1548a747c0
Tahsin <tbisk@web.de>
2021-07-29 08:03:21 +0200

    gelöst

--- challenge_01.js
+++ challenge_01.js
@@ -8,3 +8,11 @@ Berechnen Sie die Summe der Zahlen.
 Geben Sie die Summe der Zahlen in die Konsole aus:
 "Die Summe der Zahlen ist: summe"
 */
+zahlA = prompt("Bitte erste Zahl eingeben:");
+zahlB = prompt("Bitte zweite Zahl eingeben:")
+console.log(zahlA);
+console.log(zahlB);
+zahlA = Number(zahlA);
+zahlB = Number(zahlB);
+let summe = zahlA + zahlB;
+console.log("Die Summe beider Zahlen ergibt: " + summe);
